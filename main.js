3babf74f42b7e0e22010111f345d14e574096b90
Gigglebytes <35287922+Gigglebytes@users.noreply.github.com>
2018-01-24 16:43:08 -0800

    Basic Animation and multiple Ball Creation

--- main.js
+++ main.js
@@ -24,13 +24,63 @@ function Ball(x, y, velX, velY, color, size) {
   this.color = color;
   this.size = size;
 }
-//add a draw() method to the Ball()'s prototype
+//add a draw() method to the Ball()'s prototype i.e. all future balls
 Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
+  ctx.beginPath();//begin drawing on <canvas>
+  ctx.fillStyle = this.color;//color of ball
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);//draw circle
+  ctx.fill();//end drawing
 }
+
+//update method for ball movement
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {//right
+    this.velX = -(this.velX);//reverse x direction
+  }
+
+  if ((this.x - this.size) <= 0) {//left
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {//top
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {//bottom
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+var balls = [];
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 25) {
+    var ball = new Ball(
+      random(0,width),
+      random(0,height),
+      random(-7,7),
+      random(-7,7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+      random(10,20)
+    );
+    balls.push(ball);
+  }
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  requestAnimationFrame(loop);
+}
+//begin animation
+loop();
 
 //test code for for javascript console
 /*
